5425e137e545140174e360ab109fb3d6dc00b29f
Nadir Akhtar <nadirakhtar123@gmail.com>
2019-11-02 00:11:35 -0700

    In the middle of something

--- contracts/OptionsContract.sol
+++ contracts/OptionsContract.sol
@@ -1,8 +1,10 @@
 pragma solidity 0.5.10;
 
+import "./OptionsFactory.sol";
+import "./UniswapFactoryInterface.sol";
+import "./UniswapExchangeInterface.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "./OptionsFactory.sol";
 
 contract OptionsContract is ERC20 {
     struct Repo {
@@ -10,6 +12,10 @@ contract OptionsContract is ERC20 {
         uint256 debt;
         address payable owner;
     }
+
+    UniswapFactoryInterface constant UNISWAP_FACTORY = UniswapFactoryInterface(
+        0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95
+    );
 
     Repo[] repos;
 
@@ -22,13 +28,15 @@ contract OptionsContract is ERC20 {
     uint256 windowSize; // amt of seconds before expiry tht a person has to exercise
     uint256 amtOptionsExercised;
 
-    uint16 public collateralizationRatio = 1.6; //(need to be able have 1 decimal place)
+    uint16 public collateralizationRatio = 16; //(need to be able have 1 decimal place)
 
     IERC20 public collateral;
+    UniswapExchangeInterface public collateralExchange;
     IERC20 public underlying;
     uint256 public strikePrice; //depending on underlying type need to be able to handle decimal places
     IERC20 public strikeAsset;
     IERC20 public payout;
+    UniswapExchangeInterface public payoutExchange;
     uint256 public expiry;
 
     constructor(
@@ -42,6 +50,9 @@ contract OptionsContract is ERC20 {
         public
     {
         collateral = _collateral;
+        if (!isETH(collateral)) {
+            UNISWAP_FACTORY.getExchange(address(collateral));
+        }
         underlying = _underlying;
         strikePrice = _strikePrice;
         strikeAsset = _strikeAsset;
@@ -49,12 +60,24 @@ contract OptionsContract is ERC20 {
         expiry = _expiry;
     }
 
-    function getRepos(address owner) public view returns (uint[] memory);
+    // function getRepos(address owner) public view returns (uint[] memory);
 
-    function isEthCollateral() public view returns (bool) {
-        return collateral == IERC20(0);
+
+    function exercise(uint256 _amtToExercise) public {
+        // 1. before exercise window: revert
+        require(now >= expiry - windowSize, "Too early to exercise");
+        require(now < expiry, "Beyond exercise time");
+
+
+        // 2. during exercise window: exercise
+
+
+        // 3. after: TBD (but don't allow exercise)
+
     }
 
-
+    function isETH(IERC20 _ierc20) public pure returns (bool) {
+        return _ierc20 == IERC20(0);
+    }
 
 }
