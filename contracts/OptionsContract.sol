9d23e884ad7c17cb4a2b6dcea278836ffac7a839
Nadir Akhtar <nadirakhtar123@gmail.com>
2019-11-02 01:27:13 -0700

    Finish add collateral

--- contracts/OptionsContract.sol
+++ contracts/OptionsContract.sol
@@ -5,8 +5,10 @@ import "./UniswapFactoryInterface.sol";
 import "./UniswapExchangeInterface.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract OptionsContract is ERC20 {
+    using SafeMath for uint256;
     struct Repo {
         uint256 collateral;
         uint256 debt;
@@ -26,7 +28,7 @@ contract OptionsContract is ERC20 {
     mapping (address => uint256) amtExercised;
     bool optionType; // 1 is American / 0 is European
     uint256 windowSize; // amt of seconds before expiry tht a person has to exercise
-    uint256 amtOptionsExercised;
+    uint256 totalExercised; // total collateral withdrawn from contract balance
 
     uint16 public collateralizationRatio = 16; //(need to be able have 1 decimal place)
 
@@ -51,25 +53,59 @@ contract OptionsContract is ERC20 {
     {
         collateral = _collateral;
         if (!isETH(collateral)) {
-            UNISWAP_FACTORY.getExchange(address(collateral));
+            // go to Uniswap for the appropriate exchange
+            collateralExchange = UniswapExchangeInterface(
+                UNISWAP_FACTORY.getExchange(address(collateral))
+            );
+
+            // if address(0), uniswap doesn't have an exchange
+            if (address(collateralExchange) == address(0)) {
+                revert("No collateral exchange");
+            }
         }
+
         underlying = _underlying;
         strikePrice = _strikePrice;
         strikeAsset = _strikeAsset;
         payout = _payout;
+        if (!isETH(payout)) {
+            // same as above for collateral
+            payoutExchange = UniswapExchangeInterface(
+                UNISWAP_FACTORY.getExchange(address(payout))
+            );
+
+            if (address(payoutExchange) == address(0)) {
+                revert("No payout exchange");
+            }
+        }
+
         expiry = _expiry;
+    }
+
+    function addETHCollateral(uint256 _repoNum) public payable returns (uint256) {
+        return _addCollateral(_repoNum, msg.value);
+    }
+
+    function addERC20Collateral(uint256 _repoNum, uint256 _amt) public returns (uint256) {
+        require(collateral.transferFrom(msg.sender, address(this), _amt));
+
+        return _addCollateral(_repoNum, _amt);
     }
 
     // function getRepos(address owner) public view returns (uint[] memory);
 
 
-    function exercise(uint256 _amtToExercise) public {
+    function exercise(uint256 _pTokens) public {
         // 1. before exercise window: revert
         require(now >= expiry - windowSize, "Too early to exercise");
         require(now < expiry, "Beyond exercise time");
 
-
         // 2. during exercise window: exercise
+        /// 2.1 ensure person calling has enough pTokens
+        /// 2.2 check they have corresponding number of underlying
+        /// 2.3 transfer in underlying and pTokens
+        /// 2.4 sell enough collateral to get strikePrice * pTokens number of payoutTokens
+        //// 2.4.1 func on uniswap which performs sell and transfer to given user
 
 
         // 3. after: TBD (but don't allow exercise)
@@ -80,4 +116,15 @@ contract OptionsContract is ERC20 {
         return _ierc20 == IERC20(0);
     }
 
+    function _addCollateral(uint256 _repoNum, uint256 _amt) private returns (uint256) {
+        require(now < expiry, "Options contract expired");
+
+        Repo storage repo = repos[_repoNum];
+
+        repo.collateral = repo.collateral.add(_amt);
+
+        totalCollateral = totalCollateral.add(_amt);
+
+        return repo.collateral;
+    }
 }
