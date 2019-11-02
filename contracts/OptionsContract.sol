8e72ddeed5255a93ed93226af41bd3b7e243421b
Nadir Akhtar <nadirakhtar123@gmail.com>
2019-11-01 23:17:49 -0700

    Factory and options contract constructor

--- contracts/OptionsContract.sol
+++ contracts/OptionsContract.sol
@@ -1,5 +1,32 @@
 pragma solidity 0.5.10;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
 contract OptionsContract {
+    IERC20 public collateral;
+    IERC20 public underlying;
+    uint256 public strikePrice;
+    IERC20 public strikeAsset;
+    IERC20 public payout;
+    uint256 public expiry;
+
+    constructor(
+        IERC20 _collateral,
+        IERC20 _underlying,
+        uint256 _strikePrice,
+        IERC20 _strikeAsset,
+        IERC20 _payout,
+        uint256 _expiry
+    )
+        public
+    {
+        collateral = _collateral;
+        underlying = _underlying;
+        strikePrice = _strikePrice;
+        strikeAsset = _strikeAsset;
+        payout = _payout;
+        expiry = _expiry;
+    }
 
 }
--- contracts/OptionsFactory.sol
+++ contracts/OptionsFactory.sol
@@ -1,5 +1,77 @@
 pragma solidity 0.5.10;
 
-contract OptionsFactory {
+import "./StringComparator.sol";
+import "./OptionsContract.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+
+contract OptionsFactory is Ownable {
+    using StringComparator for string;
+
+    // keys saved in front-end -- look at the docs if needed
+    mapping (string => IERC20) public tokens;
+
+    event AssetAdded(string indexed asset, address indexed addr);
+    event AssetChanged(string indexed asset, address indexed addr);
+    event AssetDeleted(string indexed asset);
+
+    function createOptionsContract(
+        string memory _collateralType,
+        string memory _underlyingType,
+        uint256 _strikePrice,
+        string memory _strikeAsset,
+        string memory _payoutType,
+        uint256 _expiry
+    )
+        public
+        returns (address)
+    {
+        require(supportsAsset(_collateralType), "Collateral type not supported");
+        require(supportsAsset(_underlyingType), "Underlying type not supported");
+        require(supportsAsset(_strikeAsset), "Strike asset type not supported");
+        require(supportsAsset(_payoutType), "Payout type not supported");
+
+        OptionsContract optionsContract = new OptionsContract(
+            tokens[_collateralType],
+            tokens[_underlyingType],
+            _strikePrice,
+            tokens[_strikeAsset],
+            tokens[_payoutType],
+            _expiry
+        );
+
+        return address(optionsContract);
+    }
+
+    function addAsset(string memory _asset, address _addr) public onlyOwner {
+        require(tokens[_asset] == IERC20(0), "Asset already added");
+        require(_addr != address(0), "Cannot set to address(0)");
+
+        tokens[_asset] = IERC20(_addr);
+        emit AssetAdded(_asset, _addr);
+    }
+
+    function changeAsset(string memory _asset, address _addr) public onlyOwner {
+        require(tokens[_asset] != IERC20(0), "Trying to replace a non-existent asset");
+        require(_addr != address(0), "Cannot set to address(0)");
+
+        tokens[_asset] = IERC20(_addr);
+        emit AssetChanged(_asset, _addr);
+    }
+
+    function deleteAsset(string memory _asset) public onlyOwner {
+        require(tokens[_asset] != IERC20(0), "Trying to delete a non-existent asset");
+
+        tokens[_asset] = IERC20(0);
+        emit AssetDeleted(_asset);
+    }
+
+    function supportsAsset(string memory _collateralType) public view returns (bool) {
+        if (_collateralType.compareStrings("ETH")) {
+            return true;
+        }
+
+        return tokens[_collateralType] != IERC20(0);
+    }
 }
