7ab99cda74ce1db12e40692847fe4a6655bd938d
aparna krishnan <aparnalocked@gmail.com>
2019-12-02 13:34:35 -0700

    readded tests for now

---
+++ contracts/TestSmartContracts.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+
+/**
+* This file exists purely to import contracts we want to use in our tests, indicating to Truffle these contracts should
+* be compiled so that we may artifacts.require them in our tests to be constructed.
+*/
+contract TestSmartContracts {
+}
