1a9423f480fccd515387095dfcb8f844d285c75d
Lisandro Corbalan <lmcorbalan@gmail.com>
2020-01-09 12:04:26 -0300

    Add tests for new expiry and windowSize checks

--- test/optionsFactory.test.ts
+++ test/optionsFactory.test.ts
@@ -11,7 +11,9 @@ const MockUniswapFactory = artifacts.require('MockUniswapFactory');
 const OptionsContract = artifacts.require('OptionsContract');
 const truffleAssert = require('truffle-assertions');
 
-import { getUnixTime, addMonths } from 'date-fns';
+import { getUnixTime, addMonths, addSeconds, fromUnixTime } from 'date-fns';
+
+const { expectRevert, time } = require('@openzeppelin/test-helpers');
 
 contract('OptionsFactory', accounts => {
   const creatorAddress = accounts[0];
@@ -184,6 +186,48 @@ contract('OptionsFactory', accounts => {
   });
 
   describe('#createOptionsContract()', () => {
+    it('should not allow to create an expided new options contract', async () => {
+      const expiredExpiry = getUnixTime(new Date());
+
+      await expectRevert(
+        optionsFactory.createOptionsContract(
+          'ETH',
+          -'18',
+          'ETH',
+          -'18',
+          -'17',
+          '90',
+          -'18',
+          'ETH',
+          expiredExpiry,
+          expiredExpiry,
+          { from: creatorAddress, gas: '4000000' }
+        ),
+        'Cannot create an expired option'
+      );
+    });
+
+    it('should not allow to create a new options contract where windowSize is bigger than expiry', async () => {
+      const bigWindowSize = getUnixTime(addSeconds(fromUnixTime(expiry), 1));
+
+      await expectRevert(
+        optionsFactory.createOptionsContract(
+          'ETH',
+          -'18',
+          'ETH',
+          -'18',
+          -'17',
+          '90',
+          -'18',
+          'ETH',
+          expiry,
+          bigWindowSize,
+          { from: creatorAddress, gas: '4000000' }
+        ),
+        'Invalid _windowSize'
+      );
+    });
+
     it('should create a new options contract correctly', async () => {
       const result = await optionsFactory.createOptionsContract(
         'ETH',
